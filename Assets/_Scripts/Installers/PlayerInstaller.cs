ba27c707f0914003bfa0dde40d58d828f3af4afa
Yaroslav Oslam <ggStrider1@gmail.com>
2025-01-22 17:30:33 +0200

    changed logic for interact system. +minor updates

--- Assets/_Scripts/Installers/PlayerInstaller.cs
+++ Assets/_Scripts/Installers/PlayerInstaller.cs
@@ -13,5 +13,21 @@ namespace _Scripts.Installers
             Container.BindInterfacesAndSelfTo<InputReader>().AsSingle().NonLazy();
             Container.Bind<Camera>().FromInstance(_playerCamera).AsSingle();
         }
+        
+        #if UNITY_EDITOR
+        private void Reset()
+        {
+            if (_playerCamera == null)
+            {
+                Debug.Log("Trying to set _playerCamera into Camera.main");
+                _playerCamera = Camera.main;
+
+                if (_playerCamera == null)
+                {
+                    Debug.LogWarning("Camera main is null! Set _playerCamera in PlayerInstaller");
+                }
+            }
+        }
+        #endif
     }
 }
---
+++ Assets/_Scripts/Interact/InteractToggleGameObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Interact
+{
+    public class InteractToggleGameObject : MonoBehaviour, IInteractableByPointer
+    {
+        [SerializeField] private GameObject _gameObjectToToggle;
+
+        // He -> https://github.com/ShrAmix asked me to add this variable
+        [SerializeField] private uint _price;
+        
+        public void OnInteractByPointer()
+        {
+            _gameObjectToToggle.SetActive(true);
+        }
+
+        public void OnStopInteractByPointer()
+        {
+            _gameObjectToToggle.SetActive(false);
+        }
+    }
+}
